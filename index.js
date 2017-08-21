c4244073ed641b1289357ca79066d075c36df1dc
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 14:33:11 +0400

    " -13 +13"

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = function(babel) {
                     if (path.node.argument.callee.type === "MemberExpression") {
                         console.log("MemberExpression !");
                     } else {
-                        console.log(path.node.argument);
+                        console.log(path.node.argument.arguments);
                         path.node.argument = t.NewExpression(t.Identifier("Promise"), [
                             t.ArrowFunctionExpression([t.Identifier("resolve"), t.Identifier("reject")], t.BlockStatement([]))
                         ]);
