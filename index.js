43d9f2adbf8169988278afa88dbec43e14a703ca
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 14:12:46 +0400

    " -9,4 +9,4"

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ module.exports = function(babel) {
                 let comment1 = path.parentPath.node.leadingComments;
                 let comment2 = path.parentPath.parentPath.node.leadingComments;
                 if (comment1 !== null || comment2 !== null) {
-                    //const comment =
-                    comment1[0].value.match(/@([^ ]*)/)[1] == undefined
-                        ? comment2[0].value.match(/@([^ ]*)/)[1]
-                        : comment1[0].value.match(/@([^ ]*)/)[1];
+                    const comment =
+                        comment1[0].value.match(/@([^ ]*)/)[1] == undefined
+                            ? comment2[0].value.match(/@([^ ]*)/)[1]
+                            : comment1[0].value.match(/@([^ ]*)/)[1];
                     console.log(comment);
                 }
                 // if (comment !== null) {
