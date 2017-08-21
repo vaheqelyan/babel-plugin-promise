9235665a924100ef21832f9c603939f2fdc093ad
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 15:42:27 +0400

    " -11 +11,2"

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ module.exports = function(babel) {
                     const value = comment[0].value.match(/@([^ ]*)/)[1];
 
                     if (path.node.argument.callee.type === "MemberExpression") {
-                        console.log("MemberExpression !");
+                        let getCallbackArguments =
+                            path.node.argument.arguments[path.node.argument.arguments.length - 1].arguments;
                     } else {
                         let getCallbackArguments =
                             path.node.argument.arguments[path.node.argument.arguments.length - 1].arguments;
