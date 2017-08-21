4d67218470de49114da60e420e4d398ad5821ebe
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 13:54:23 +0400

    " -7 +7"

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function(babel) {
         visitor: {
             CallExpression(path) {
                 //let comment = path.parentPath.node.leadingComments[0].value.match(/@([^ ]*)/)[1];
-                console.log(path.parentPath.leadingComments);
+                console.log(path.parentPath.node.leadingComments);
                 // path.replaceWithSourceString(`function add(a, b) {
                 //     return a + b;
                 //   }`);
