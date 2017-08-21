ce8ca0ab5974913838b1b2c7fd004f4c25efe017
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 13:10:35 +0400

    " -7 +7"

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function(babel) {
         visitor: {
             Identifier(path) {
                 if (path.node.name === "async") {
-                    console.log(path.parentPath.node.object);
+                    console.log(path.parentPath.node.object.calee);
                 }
             }
         }
