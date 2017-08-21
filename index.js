e60407457a2e6a37deec1d45ae5ce512437fa1e1
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 13:43:46 +0400

    " -7,3 +7,5"

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ module.exports = function(babel) {
         visitor: {
             ExpressionStatement(path) {
                 //let comment = path.node.leadingComments[0].value.match(/@([^ ]*)/)[1];
-
-                path.insertBefore(t.expressionStatement(t.stringLiteral("Because I'm easy come, easy go.")));
-                path.insertAfter(t.expressionStatement(t.stringLiteral("A little high, little low.")));
+                path.replaceWithSourceString(`function add(a, b) {
+                    return a + b;
+                  }`);
+                //path.insertBefore(t.expressionStatement(t.stringLiteral("Because I'm easy come, easy go.")));
+                //path.insertAfter(t.expressionStatement(t.stringLiteral("A little high, little low.")));
             }
         }
     };
