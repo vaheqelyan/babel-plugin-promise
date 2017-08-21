45776fec2bc39ef52cea794c03f5d26de220a503
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 14:08:12 +0400

    " -8,2 +8,2"

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ module.exports = function(babel) {
             AwaitExpression(path) {
                 let comment = path.parentPath.parentPath.node.leadingComments;
                 if (comment !== null) {
-                    comment = comment[0].value.match(/@([^ ]*)/)[1];
-                    console.log(comment);
+                    // comment = comment[0].value.match(/@([^ ]*)/)[1];
+                    // console.log(comment);
                 }
             }
         }
