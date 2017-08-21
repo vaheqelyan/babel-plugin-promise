2acd8028485ee25a22ddd65153abec5508219f69
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 13:13:49 +0400

    " -6 +6"

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = function(babel) {
     return {
         visitor: {
             AssignmentExpression(path) {
-                console.log(path.node.left);
+                console.log(path.node.left.node.name);
             }
         }
     };
