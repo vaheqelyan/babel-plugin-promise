e5276a5c9c4a52a6a90683217001416ce0849e51
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 14:27:31 +0400

    " -14 +14"

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = function(babel) {
                         console.log("MemberExpression !");
                     } else {
                         path.node.argument = t.NewExpression(t.Identifier("Promise"), [
-                            t.ArrowFunctionExpression([t.Identifier("resolve"), t.Identifier("reject")], t.BlockStatement([]))
+                            t.ArrowFunctionExpression([t.Identifier("resolve"), t.Identifier("reject")], t.BlockStatement([t.ExpressionStatement()]))
                         ]);
                     }
                 }
