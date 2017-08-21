538a1573aa93e82df61d4c8a292dad5c1a96adcf
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 13:37:04 +0400

    " -5,9 +5,2"

--- index.js
+++ index.js
@@ -2,15 +2,8 @@ module.exports = function(babel) {
     const t = babel.types;
     return {
         visitor: {
-            AssignmentExpression(path) {
-                var isPromise = path.node.left.name;
-                let promisedName = String;
-                if (isPromise === "promise") {
-                    let arrow = path.node.right;
-                    promisedName = arrow.params[0].name;
-                    path.node.left.name = promisedName;
-                    arrow.params.length = 0;
-                }
+            CallExpression(path) {
+                console.log(path);
             }
         }
     };
