d6bc718e725f597e0584b01c29848c5909acd123
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 13:38:09 +0400

    " -6 +6"

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = function(babel) {
     return {
         visitor: {
             CallExpression(path) {
-                console.log(path.node);
+                console.log(path.node.leadingComments);
             }
         }
     };
