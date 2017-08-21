00ea1769f1562feed4dd8ddbd404fd0e828d9865
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 15:00:07 +0400

    " -14,3 +14,2  -41,3 +40"

--- index.js
+++ index.js
@@ -11,9 +11,8 @@ module.exports = function(babel) {
                         console.log("MemberExpression !");
                     } else {
                         let getCallbackArguments =
-                            path.node.argument.arguments[
-                                path.node.argument.arguments.length - 1
-                            ].arguments;
+                            path.node.argument.arguments[path.node.argument.arguments.length - 1]
+                                .arguments;
                         path.node.argument.arguments[
                             path.node.argument.arguments.length - 1
                         ] = t.ArrowFunctionExpression(
@@ -38,9 +37,7 @@ module.exports = function(babel) {
                         path.node.argument = t.NewExpression(t.Identifier("Promise"), [
                             t.ArrowFunctionExpression(
                                 [t.Identifier("resolve"), t.Identifier("reject")],
-                                t.BlockStatement([
-                                    t.ExpressionStatement(path.node.argument)
-                                ])
+                                t.BlockStatement([t.ExpressionStatement(path.node.argument)])
                             )
                         ]);
                     }
