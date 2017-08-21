6b8c33a72f1d4eff2bbde251a250e877b897ca66
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 14:15:48 +0400

    " -9 +9"

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = function(babel) {
                 let comment = path.parentPath.node.trailingComments;
                 if (comment) {
                     const value = comment[0].value.match(/@([^ ]*)/)[1];
-                    const type = path.node.argument.callee;
+                    const type = path.node.argument.callee.type;
                 }
                 // if (comment !== null) {
                 //     console.log(comment);
