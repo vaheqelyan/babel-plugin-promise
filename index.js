b36d65e75d10ed821edd063db5bafa8c1543cf8c
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 13:55:26 +0400

    " -10 +10,3  -12,3 +13,0"

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ module.exports = function(babel) {
                 let comment = path.parentPath.node.leadingComments;
                 if (comment !== null) {
                     comment = comment[0].value.match(/@([^ ]*)/)[1];
-                    console.log(path.node.type);
+                    // path.replaceWithSourceString(`function add(a, b) {
+                    //     return a + b;
+                    //   }`);
                 }
-                // path.replaceWithSourceString(`function add(a, b) {
-                //     return a + b;
-                //   }`);
             }
         }
     };
