3feab922d861096306988119a4b48e02b0b4b209
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 13:48:31 +0400

    " -6 +6"

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = function(babel) {
     return {
         visitor: {
             ExpressionStatement(path) {
-                path.parentPath.push(t.expressionStatement(t.stringLiteral("Because I'm easy come, easy go.")));
+                path.parentPath.node.push(t.expressionStatement(t.stringLiteral("Because I'm easy come, easy go.")));
                 //let comment = path.node.leadingComments[0].value.match(/@([^ ]*)/)[1];
                 //path.insertBefore(t.expressionStatement(t.stringLiteral("Because I'm easy come, easy go.")));
                 //path.insertAfter(t.expressionStatement(t.stringLiteral("A little high, little low.")));
