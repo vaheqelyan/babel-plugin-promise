fff429baa23dd88bba68d41228b30fa4cdaa95d5
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 13:51:34 +0400

    " -5 +5"

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function(babel) {
     const t = babel.types;
     return {
         visitor: {
-            ExpressionStatement(path) {
+            CallExpression(path) {
                 //let comment = path.node.leadingComments[0].value.match(/@([^ ]*)/)[1];
                 path.replaceWith(t.callExpression(t.memberExpression(t.identifier("mori"), t.identifier("vector")), path.node.elements));
             }
