93b00e7cb7b4eebfbafe310ae77bc874a04041f5
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 16:11:53 +0400

    " -9 +9"

--- test.js
+++ test.js
@@ -6,6 +6,6 @@ async function name() {
     await findOne(callback(err, user));
 
     async function asd() {
-        await hello(null, callback(err, asd)); //@asd
+        await hello(null, callback(err, asd));
     }
 }
