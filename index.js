ae41c1453ea18031ea2963d7dcbdf9f45ea2324d
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 13:55:00 +0400

    " -7 +7,4"

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ module.exports = function(babel) {
         visitor: {
             CallExpression(path) {
                 //let comment = path.parentPath.node.leadingComments[0].value.match(/@([^ ]*)/)[1];
-                console.log(path.parentPath.node.leadingComments);
+                let comment = path.parentPath.node.leadingComments;
+                if (comment !== null) {
+                    comment = comment[0].value.match(/@([^ ]*)/)[1];
+                }
                 // path.replaceWithSourceString(`function add(a, b) {
                 //     return a + b;
                 //   }`);
