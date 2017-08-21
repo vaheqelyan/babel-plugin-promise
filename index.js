1a14208366fd19aeb258ff8438153f89d555579e
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 13:00:39 +0400

    " -3,7 +2,0"

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 module.exports = function(babel) {
     const t = babel.types;
-
-    function moriMethod(name) {
-        const expr = t.memberExpression(t.identifier("mori"), t.identifier(name));
-        expr.isClean = true;
-        return expr;
-    }
-
     return {
         visitor: {}
     };
