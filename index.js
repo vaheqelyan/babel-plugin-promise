8a954d79a136949293107668981906dd85953a88
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 14:09:54 +0400

    " -8 +8,2"

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ module.exports = function(babel) {
             AwaitExpression(path) {
                 let comment1 = path.parentPath.node.leadingComments;
                 let comment2 = path.parentPath.parentPath.node.leadingComments;
-                console.log(comment);
+                console.log(comment1);
+                console.log(comment2);
                 // if (comment !== null) {
                 //     console.log(comment);
                 //     // comment = comment[0].value.match(/@([^ ]*)/)[1];
