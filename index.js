56ceec4be28d6deba14852fc00c1eda79086f6d9
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 13:31:03 +0400

    " -11 +11"

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = function(babel) {
                 if (isPromise === "promise") {
                     let arrow = path.node.right;
                     promisedName = arrow.params[0].name;
-                    //path.node.left.name = promisedName;
+                    path.node.left.name = promisedName;
                 }
             }
         }
