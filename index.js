c0366aa7518f0f64d50a4d92f428052d7c28b5c0
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 14:02:31 +0400

    " -6 +6"

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = function(babel) {
     return {
         visitor: {
             AwaitExpression(path) {
-                console.log(path);
+                console.log(path.get("CallExpression"));
                 // let comment = path.parentPath.node.leadingComments;
                 // if (comment !== null) {
                 //     comment = comment[0].value.match(/@([^ ]*)/)[1];
