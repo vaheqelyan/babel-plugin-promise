4cd7a0d74eaff1122b1e481177cb91c0a172f7be
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 14:17:32 +0400

    " -9,5 +9"

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ module.exports = function(babel) {
                 let comment = path.parentPath.node.trailingComments;
                 if (comment) {
                     const value = comment[0].value.match(/@([^ ]*)/)[1];
-                    if (path.node.callee.type === "MemberExpression") {
-                        console.log("MemberExpression !");
-                    } else {
-                        console.log("iden");
-                    }
+                    console.log(path.node);
                 }
                 // if (comment !== null) {
                 //     console.log(comment);
