dffe160e24f96c8793a6f698e9699896bab0b6fd
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 13:46:39 +0400

    " -5,0 +6"

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = function(babel) {
     return {
         visitor: {
             ExpressionStatement(path) {
+                path.get("body").unshiftContainer("body", t.expressionStatement(t.stringLiteral("before")));
                 //let comment = path.node.leadingComments[0].value.match(/@([^ ]*)/)[1];
                 //path.insertBefore(t.expressionStatement(t.stringLiteral("Because I'm easy come, easy go.")));
                 //path.insertAfter(t.expressionStatement(t.stringLiteral("A little high, little low.")));
