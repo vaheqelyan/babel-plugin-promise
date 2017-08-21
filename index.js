d1cf9800d2173106ddb00fb4d67ee3ec67f9ca1b
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 15:00:25 +0400

    " -1,5 +1,2  -8,14 +5,4  -23,11 +10,2  -36,20 +14,2  -57,33 +17,7  -92,26 +26,6"

--- index.js
+++ index.js
@@ -1,120 +1,34 @@
-module.exports = function(
-    babel
-) {
-    const t =
-        babel.types;
+module.exports = function(babel) {
+    const t = babel.types;
     return {
         visitor: {
-            AwaitExpression(
-                path
-            ) {
-                let comment =
-                    path
-                        .parentPath
-                        .node
-                        .trailingComments;
-                if (
-                    comment
-                ) {
-                    const value = comment[0].value.match(
-                        /@([^ ]*)/
-                    )[1];
+            AwaitExpression(path) {
+                let comment = path.parentPath.node.trailingComments;
+                if (comment) {
+                    const value = comment[0].value.match(/@([^ ]*)/)[1];
 
-                    if (
-                        path
-                            .node
-                            .argument
-                            .callee
-                            .type ===
-                        "MemberExpression"
-                    ) {
-                        console.log(
-                            "MemberExpression !"
-                        );
+                    if (path.node.argument.callee.type === "MemberExpression") {
+                        console.log("MemberExpression !");
                     } else {
                         let getCallbackArguments =
-                            path
-                                .node
-                                .argument
-                                .arguments[
-                                path
-                                    .node
-                                    .argument
-                                    .arguments
-                                    .length -
-                                    1
-                            ]
-                                .arguments;
-                        path.node.argument.arguments[
-                            path
-                                .node
-                                .argument
-                                .arguments
-                                .length -
-                                1
-                        ] = t.ArrowFunctionExpression(
+                            path.node.argument.arguments[path.node.argument.arguments.length - 1].arguments;
+                        path.node.argument.arguments[path.node.argument.arguments.length - 1] = t.ArrowFunctionExpression(
                             getCallbackArguments,
-                            t.BlockStatement(
-                                [
-                                    t.IfStatement(
-                                        t.Identifier(
-                                            "err"
-                                        ),
-                                        t.ExpressionStatement(
-                                            t.CallExpression(
-                                                t.Identifier(
-                                                    "reject"
-                                                ),
-                                                [
-                                                    t.Identifier(
-                                                        "err"
-                                                    )
-                                                ]
-                                            )
-                                        )
-                                    ),
-                                    t.ExpressionStatement(
-                                        t.CallExpression(
-                                            t.Identifier(
-                                                "resolve"
-                                            ),
-                                            [
-                                                t.Identifier(
-                                                    "user"
-                                                )
-                                            ]
-                                        )
-                                    )
-                                ]
-                            )
+                            t.BlockStatement([
+                                t.IfStatement(
+                                    t.Identifier("err"),
+                                    t.ExpressionStatement(t.CallExpression(t.Identifier("reject"), [t.Identifier("err")]))
+                                ),
+                                t.ExpressionStatement(t.CallExpression(t.Identifier("resolve"), [t.Identifier("user")]))
+                            ])
                         );
 
-                        path.node.argument = t.NewExpression(
-                            t.Identifier(
-                                "Promise"
-                            ),
-                            [
-                                t.ArrowFunctionExpression(
-                                    [
-                                        t.Identifier(
-                                            "resolve"
-                                        ),
-                                        t.Identifier(
-                                            "reject"
-                                        )
-                                    ],
-                                    t.BlockStatement(
-                                        [
-                                            t.ExpressionStatement(
-                                                path
-                                                    .node
-                                                    .argument
-                                            )
-                                        ]
-                                    )
-                                )
-                            ]
-                        );
+                        path.node.argument = t.NewExpression(t.Identifier("Promise"), [
+                            t.ArrowFunctionExpression(
+                                [t.Identifier("resolve"), t.Identifier("reject")],
+                                t.BlockStatement([t.ExpressionStatement(path.node.argument)])
+                            )
+                        ]);
                     }
                 }
             }
