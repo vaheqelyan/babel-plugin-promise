b790211e2b13bc825e242c97438fc9fe2e2ed928
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 16:13:49 +0400

    " -0,0 +1,6"

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
+promise = myaync =>
+    setTimeout(() => {
+        // переведёт промис в состояние fulfilled с результатом "result"
+        resolve("result");
+    }, 1000);
+
 async function name() {
     asdf;
     asdfasdf;
