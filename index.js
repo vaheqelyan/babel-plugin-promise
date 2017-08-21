9e855aa3782bd9e376667200507d0a98f95ba471
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 13:53:08 +0400

    " -6,4 +6,5"

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ module.exports = function(babel) {
     return {
         visitor: {
             CallExpression(path) {
-                //let comment = path.node.leadingComments[0].value.match(/@([^ ]*)/)[1];
-                path.replaceWithSourceString(`function add(a, b) {
-                    return a + b;
-                  }`);
+                let comment = path.node.leadingComments[0].value.match(/@([^ ]*)/)[1];
+                console.log(comment);
+                // path.replaceWithSourceString(`function add(a, b) {
+                //     return a + b;
+                //   }`);
             }
         }
     };
