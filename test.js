2e338d355e3f5c5f1e2c78f240053612bc8c0191
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 15:42:09 +0400

    " -9 +9"

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ async function name() {
     await findOne(callback(err, user));
 
     async function asd() {
-        await findOne({ name: "vahe", _id: "123123123" }, callback(err, profile)); //@promise
+        await User.findOne({ name: "vahe", _id: "123123123" }, callback(err, profile)); //@promise
     }
     await asd();
 }
