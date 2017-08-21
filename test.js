8c8c02b972a0cdee92548a7e76148beec866b6cd
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-21 14:07:39 +0400

    " -5,2 +5,2  -10 +9,0"

--- test.js
+++ test.js
@@ -2,10 +2,9 @@ async function name() {
     asdf;
     asdfasdf;
     asdfasdf;
-    
-    function asd(){
+
+    async function asd() {
         //@promise
         let name = await findOne(arg1, callback(err, user)).async();
     }
-    
 }
