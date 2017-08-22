afcd9581349fad34b752c54fe674b3016fb4f152
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-22 15:44:00 +0400

    " -45 +45"

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ module.exports = function(babel) {
                         } else {
                             path.node.argument.arguments[path.node.argument.arguments.length - 1] = t.ArrowFunctionExpression(
                                 getCallbackArguments,
-                                t.BlockStatement([t.ExpressionStatement(t.CallExpression(t.Identifier("resolve"), [t.Identifier(getCallbackArguments[0].name)]))])
+                                t.BlockStatement([t.ExpressionStatement(t.CallExpression(t.Identifier("resolve"), [t.Identifier(getCallbackArguments[1].name)]))])
                             );
                         }
                         //path.node.argument.arguments[path.node.argument.arguments.length - 1] = t.ArrowFunctionExpression(getCallbackArguments, t.BlockStatement([]));
