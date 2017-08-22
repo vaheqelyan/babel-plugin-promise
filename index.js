3fd3140c8dfeb69b17dec0e950059af9febe6faf
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-22 17:17:41 +0400

    " -72,5 +72,5"

--- index.js
+++ index.js
@@ -69,11 +69,11 @@ module.exports = function(babel) {
                 } else if (path.parentPath.parentPath.node.trailingComments) {
                     // in case of parent
                     const value = comment[0].value.match(/@([^ ]*)/)[1];
-                    if (path.node.argument.callee.type === "MemberExpression") {
-                        let getCallbackArguments = path.node.argument.arguments[path.node.argument.arguments.length - 1].arguments;
-                        console.log(getCallbackArguments);
-                    } else {
-                    }
+                    // if (path.node.argument.callee.type === "MemberExpression") {
+                    //     let getCallbackArguments = path.node.argument.arguments[path.node.argument.arguments.length - 1].arguments;
+                    //     console.log(getCallbackArguments);
+                    // } else {
+                    // }
                 }
             }
         }
