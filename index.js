651e6b0edf89582b82619123ec6127f741ba4a9f
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-22 14:39:12 +0400

    " -32,16 +32"

--- index.js
+++ index.js
@@ -29,22 +29,7 @@ module.exports = function(babel) {
                         let getCallbackArguments =
                             path.node.argument.arguments[path.node.argument.arguments.length - 1].arguments;
 
-                        path.node.argument.arguments[path.node.argument.arguments.length - 1] = t.ArrowFunctionExpression(
-                            getCallbackArguments,
-                            t.BlockStatement([
-                                getCallbackArguments[0] &&
-                                    t.IfStatement(
-                                        t.Identifier("err"),
-                                        t.ExpressionStatement(
-                                            t.CallExpression(t.Identifier("reject"), [t.Identifier(getCallbackArguments[0].name)])
-                                        )
-                                    ),
-
-                                t.ExpressionStatement(
-                                    t.CallExpression(t.Identifier("resolve"), [t.Identifier(getCallbackArguments[1].name)])
-                                )
-                            ])
-                        );
+                        console.log(getCallbackArguments);
 
                         path.node.argument = t.NewExpression(t.Identifier("Promise"), [
                             t.ArrowFunctionExpression(
