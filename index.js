4281c6e0837cf296bb9b9f8add58a3894c805bfb
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-22 14:58:02 +0400

    " -1,53 +0,0"

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ module.exports = function(babel) {
 
                         var isError =
                             getCallbackArguments.length > 0
-                                ? (getCallbackArguments[0] !== undefined && getCallbackArguments[0].name === "err") || getCallbackArguments[0].name === "error" ? "exists" : "no"
+                                ? (getCallbackArguments[0] !== undefined && getCallbackArguments[0].name === "err") || getCallbackArguments[0].name === "error" ? true : false
                                 : "no arguments";
                         console.log(isError);
                         path.node.argument.arguments[path.node.argument.arguments.length - 1] = t.ArrowFunctionExpression(
