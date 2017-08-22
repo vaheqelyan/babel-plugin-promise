95ad5c0575d30124aae2d56079320dfb57229e84
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-22 18:33:25 +0400

    " -252,2 +252"

--- index.js
+++ index.js
@@ -249,8 +249,7 @@ module.exports = function(babel) {
                 } else {
                     const Atlin = path.node.loc.start.line;
                     let comment = path.parentPath.parentPath.parentPath.node.trailingComments.value;
-                    const value = comment[0].value.match(/@([^ ]*)/)[1];
-                    console.log(value);
+                    console.log(comment);
                 }
             }
         }
