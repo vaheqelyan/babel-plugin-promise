a77a341d9453c033191165b790c9193095b44ff8
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-22 14:56:08 +0400

    " -25,2 +25,5"

--- index.js
+++ index.js
@@ -22,8 +22,11 @@ module.exports = function(babel) {
                         ]);
                     } else {
                         let getCallbackArguments = path.node.argument.arguments[path.node.argument.arguments.length - 1].arguments;
-                        //var isError = (getCallbackArguments[0] !== undefined && getCallbackArguments[0].name === "err") || getCallbackArguments[0].name === "error" ? "exists" : "no";
-                        console.log(getCallbackArguments);
+                        var isError =
+                            getCallbackArguments.length > 0
+                                ? (getCallbackArguments[0] !== undefined && getCallbackArguments[0].name === "err") || getCallbackArguments[0].name === "error" ? "exists" : "no"
+                                : console.log("no arguments");
+                        console.log(isError);
                         path.node.argument.arguments[path.node.argument.arguments.length - 1] = t.ArrowFunctionExpression(
                             getCallbackArguments,
                             t.BlockStatement([
