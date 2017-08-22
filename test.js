79acf2ed29f405532993825824228f0b2d90f64b
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-22 17:26:01 +0400

    ""

--- test.js
+++ test.js
@@ -1,12 +1,7 @@
-promise = myaync =>
-    setTimeout(() => {
-        resolve("result");
-    }, 1000);
-
-async function name() {
-    let { hello, world } = await User.findOne(callback(err, user)); //@promisifyasd
-
-    async function asd() {
-        await User(null, callback(err, user)); //@promisify
+async function readFileAsync() {
+    try {
+        await HTMLAreasCollection.call(callback(err, user));
+    } catch (error) {
+        console.error(error);
     }
 }
