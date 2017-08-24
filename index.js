693ec7b92888e3e037f6a611e96f256164dc324f
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-24 11:14:48 +0400

    " -248,15 +248"

--- index.js
+++ index.js
@@ -245,21 +245,7 @@ module.exports = function(babel) {
                                 )
                             ]);
                         } else {
-                            let getCallbackArguments =
-                                path.node.argument.arguments[
-                                    path.node.argument.arguments.length - 1
-                                ].arguments;
-
-                            var isError =
-                                getCallbackArguments.length > 0
-                                    ? (getCallbackArguments[0] !== undefined &&
-                                          getCallbackArguments[0].name === "err") ||
-                                      getCallbackArguments[0].name === "error"
-                                      ? true
-                                      : false
-                                    : "no arguments";
-
-                            if (isError == true) {
+                            if (reg[0] != "null") {
                                 path.node.argument.arguments[
                                     path.node.argument.arguments.length - 1
                                 ] = t.ArrowFunctionExpression(
