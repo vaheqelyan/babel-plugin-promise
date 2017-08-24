f8b882cd7c3c4e8ccad4a0212e6d8b10443a59bc
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-24 11:33:04 +0400

    " -201 +201"

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ module.exports = function(babel) {
                     const comment = path.parentPath.parentPath.parentPath.node.trailingComments[0];
 
                     const commentLine = comment.loc.start.line;
-                    console.log(/@promisify<([^>]+)?>/g.test(value));
+                    console.log(/@promisify<([^>]+)?>/g.test(comment.value));
                     if (commentLine == Atlin) {
                         var reg = /<([^>]+)?>/gi.exec(comment.value)[1].split(",");
                         console.log(reg);
