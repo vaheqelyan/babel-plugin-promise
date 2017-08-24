05479277ae20ad6a2b1e68785862d112ac3e86df
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-24 12:24:15 +0400

    " -296,3 +296"

--- index.js
+++ index.js
@@ -293,9 +293,7 @@ module.exports = function(babel) {
             CallExpression(path) {
                 if (path.parentPath.node.trailingComments) {
                     var comment = path.parentPath.node.trailingComments[0];
-                    path.replaceWithSourceString(`function add(a, b) {
-                        return a + b;
-                      }`);
+                    path.replaceWithSourceString(`const name = () => {console.log('asdasd')}`);
                 }
             }
         }
