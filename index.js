d6550ca2e30f505a4df41a0523c97b36100eaebd
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-24 12:21:57 +0400

    " -296 +296"

--- index.js
+++ index.js
@@ -293,7 +293,7 @@ module.exports = function(babel) {
             CallExpression(path) {
                 if (path.parentPath.node.trailingComments) {
                     var comment = path.parentPath.node.trailingComments[0];
-                    path = t.FunctionDeclaration(t.Identifier("name"), t.BlockStatement([]), []);
+                    path = t.FunctionDeclaration(t.Identifier("name"), [], t.BlockStatement([]));
                 }
             }
         }
