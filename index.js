ac45651c4029199ff09dc22e4727a344b69e9047
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-24 11:10:58 +0400

    " -113 +113  -116 +116  -119 +119  -125 +125  -134 +134  -138 +138"

--- index.js
+++ index.js
@@ -110,19 +110,19 @@ module.exports = function(babel) {
                                 path.node.argument.arguments[
                                     path.node.argument.arguments.length - 1
                                 ] = t.ArrowFunctionExpression(
-                                    getCallbackArguments,
+                                    [t.Identifier(reg[0]), t.Identifier(reg[1])],
                                     t.BlockStatement([
                                         t.IfStatement(
-                                            t.Identifier(getCallbackArguments[0].name),
+                                            t.Identifier(reg[0]),
                                             t.ExpressionStatement(
                                                 t.CallExpression(t.Identifier("reject"), [
-                                                    t.Identifier(getCallbackArguments[0].name)
+                                                    t.Identifier(reg[0])
                                                 ])
                                             )
                                         ),
                                         t.ExpressionStatement(
                                             t.CallExpression(t.Identifier("resolve"), [
-                                                t.Identifier(getCallbackArguments[1].name)
+                                                t.Identifier(reg[1])
                                             ])
                                         )
                                     ])
@@ -131,11 +131,11 @@ module.exports = function(babel) {
                                 path.node.argument.arguments[
                                     path.node.argument.arguments.length - 1
                                 ] = t.ArrowFunctionExpression(
-                                    getCallbackArguments,
+                                    [t.Identifier(reg[1])],
                                     t.BlockStatement([
                                         t.ExpressionStatement(
                                             t.CallExpression(t.Identifier("resolve"), [
-                                                t.Identifier(getCallbackArguments[0].name)
+                                                t.Identifier(reg[1])
                                             ])
                                         )
                                     ])
