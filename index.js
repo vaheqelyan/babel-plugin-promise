3d66cc4b983dcd0db1cb5efb9ce9cd605e080644
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-25 13:16:45 +0400

    " -302,0 +303"

--- index.js
+++ index.js
@@ -300,6 +300,7 @@ module.exports = function(babel) {
                     var functionName = value.split(">")[1].replace(/\s/g, "");
 
                     if (reg[0] && reg[0] != "null") {
+                        console.log("ok");
                         path.node.right.arguments.push(
                             t.ArrowFunctionExpression(
                                 [t.Identifier(reg[0]), t.Identifier(reg[1])],
