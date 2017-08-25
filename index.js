27121fe418abc5d265e1d1744d1664a46e176d68
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-25 13:12:30 +0400

    " -325 +325"

--- index.js
+++ index.js
@@ -322,7 +322,7 @@ module.exports = function(babel) {
                         );
                         var args = path.node.right.arguments[0].name;
                         //args = args.filter(val => /arg/gi.test(val.name));
-                        console.log(typeof );
+                        console.log(typeof args);
                         path.parentPath.replaceWith(
                             t.FunctionDeclaration(
                                 t.Identifier(functionName),
