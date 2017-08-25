7577fb38a36724beb42948e493fb7a4999669bd6
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-25 12:41:39 +0400

    " -299,0 +300"

--- index.js
+++ index.js
@@ -297,6 +297,7 @@ module.exports = function(babel) {
                 var is = /@promisify<([^>]+)?>/g.test(value);
                 if (name === "__" && is === true) {
                     var reg = /<([^>]+)?>/gi.exec(value)[1].split(",");
+                    var functionName = value.match(/>([^ ]*)/)[1];
                     if (reg[0] && reg[0] != "null") {
                         console.log("handle reject and resolve");
                     } else {
