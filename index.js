a6825bcd82adacace926f80e4504a2be1007ec3e
vaheqelyan <vahe.araqelyan.2013@hotmail.com>
2017-08-25 13:07:58 +0400

    " -326 +326"

--- index.js
+++ index.js
@@ -323,7 +323,7 @@ module.exports = function(babel) {
                         path.parentPath.replaceWith(
                             t.FunctionDeclaration(
                                 t.Identifier(functionName),
-                                [t.Identifier(reg[0]), t.Identifier(reg[1])],
+                                [],
                                 t.BlockStatement([
                                     t.ReturnStatement(
                                         t.NewExpression(t.Identifier("Promise"), [
